e73084a9a6c717b1f5853c8aa86d095dd08d9e53
Miguel <miguelfp44@gmail.com>
2025-09-22 21:03:14 -0600

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EnvelopeComponent } from "./components/envelope/envelope.component";
 import { CommonModule } from '@angular/common';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,29 @@ export class AppComponent {
   title = 'invitational';
 
   @ViewChild('contentBelow') contentBelow!: ElementRef<HTMLDivElement>;
+  fechaEvento = new Date('2025-12-19T20:00:00'); // Cambia la fecha de tu evento
+  dias: number = 0;
+  horas: number = 0;
+  minutos: number = 0;
+  segundos: number = 0;
+
+  constructor() {
+    interval(1000).subscribe(() => this.actualizarContador());
+  }
+
+  actualizarContador() {
+    const ahora = new Date().getTime();
+    const distancia = this.fechaEvento.getTime() - ahora;
+
+    if (distancia > 0) {
+      this.dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+      this.horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+      this.segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+    } else {
+      this.dias = this.horas = this.minutos = this.segundos = 0;
+    }
+  }
 
   scrollToContent() {
     if (this.contentBelow) {
